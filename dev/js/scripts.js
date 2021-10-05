5f2bf516b8f3f9ef6826de64edf89bd82f5b6b17
matthewbruner <mab828@drexel.edu>
2021-10-05 08:47:06 -0400

    Commit

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -3,6 +3,7 @@ import { gsap } from "gsap";
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero >div",{duration:2, alpha:0, y:1000 })
-    .from ("#hero h1",{duration:0.5, alpha:0, y:"+=200"})
-    .from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"});
+mainTL.from("#hero >div",{duration:2, alpha:0, x:1000 })
+    .from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"})
+    .from ("#hero h1",{duration:0.5, alpha:0, y:"-=200"})
+    .from("#content >div",{duration:1.5, alpha:0, x:-1000 });
