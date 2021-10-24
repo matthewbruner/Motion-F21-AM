c03381e0035d24d976540aab4abf5f55d16238ab
matthewbruner <mab828@drexel.edu>
2021-10-24 16:24:03 -0400

    Preloader

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,9 +1,29 @@
 // import { gsap } from "gsap";
 import { gsap } from "gsap";
+import { GSDevTools } from "gsap/GSDevTools";
+
+
+gsap.registerPlugin(GSDevTools);
 
 const mainTL = gsap.timeline();
 
-mainTL.from("#hero >div",{duration:2, alpha:0, x:1000 })
-    .from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"})
-    .from ("#hero h1",{duration:0.5, alpha:0, y:"-=200"})
-    .from("#content >div",{duration:1.5, alpha:0, x:-1000 });
+mainTL.to("#Macaw1",{duration:1, alpha:0, scale:2})
+
+
+
+
+
+
+
+
+
+
+
+
+
+//mainTL.from("#hero >div",{duration:2, alpha:0, x:1000 })
+//   .from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"})
+//   .from ("#hero h1",{duration:0.5, alpha:0, y:"-=200"})
+//    .from("#content >div",{duration:1.5, alpha:0, x:-1000 });
+
+GSDevTools.create();
