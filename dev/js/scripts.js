6fb943eecdac0c683f33c94d4417d90d134e403e
matthewbruner <mab828@drexel.edu>
2021-11-09 23:00:44 -0500

    Nov10

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -8,42 +8,121 @@ gsap.registerPlugin(GSDevTools);
 const mainTL = gsap.timeline();
 
 
-function macawanimation (){
+function macawanimation(){
 const tl = gsap.timeline();
-tl.to("#Group1A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group2A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group3A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group4A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group5A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group6A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group7A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group8A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group9A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group10A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group11A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group12A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group13A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group14A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group15A", {duration: 0.1, autoAlpha: 0});
-tl.to("#Group16A", {duration: 0.1, autoAlpha: 0});
+
+// tl.to(".frameses", {duration:0.3, autoAlpha:1, ease:"steps(83)"});
+
+tl.to("#Group1A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group2A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group3A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group4A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group5A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group6A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group7A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group8A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group9A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group10A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group11A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group12A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group13A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group14A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group15A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group16A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group17A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group18A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group19A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group20A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group21A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group22A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group23A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group24A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group25A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group26A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group27A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group28A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group29A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group30A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group31A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group32A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group33A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group34A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group35A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group36A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group37A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group38A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group39A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group40A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group41A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group42A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group43A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group44A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group45A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group46A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group47A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group48A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group49A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group50A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group51A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group52A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group53A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group54A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group55A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group56A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group57A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group58A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group59A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group60A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group61A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group62A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group63A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group64A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group65A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group66A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group67A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group68A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group69A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group70A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group71A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group72A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group73A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group74A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group75A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group76A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group77A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group78A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group79A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group80A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group81A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group82A", {duration: 0.1, autoAlpha: 1});
+tl.to("#Group83A", {duration: 0.1, autoAlpha: 1});
 return tl;
 }
 
 
-mainTL.add(macawanimation)
 
 
 
+function exitTransition(){
+    const tl=gsap.timeline();
+    tl.to("#preloader",{autoAlpha:0,duration:2});
+    return tl;
+}
 
 
 
+function originalAni(){
+    const tl=gsap.timeline();
+tl.from("#hero >div",{duration:2, alpha:0, x:1000 })
+.from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"})
+.from ("#hero h1",{duration:0.5, alpha:0, y:"-=200"})
+.from("#content >div",{duration:1.5, alpha:0, x:-1000 });
 
+    return tl;
+}
 
-
-
-//mainTL.from("#hero >div",{duration:2, alpha:0, x:1000 })
-//   .from ("#hero h2",{duration:0.5, alpha:0, y:"+=200"})
-//   .from ("#hero h1",{duration:0.5, alpha:0, y:"-=200"})
-//    .from("#content >div",{duration:1.5, alpha:0, x:-1000 });
+mainTL.add(macawanimation())
+.add(exitTransition())
+.add(originalAni());
 
 GSDevTools.create();
