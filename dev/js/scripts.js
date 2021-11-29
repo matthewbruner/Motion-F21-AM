02aa32a5d17bb6adfae32f78c1d929c6f7244706
matthewbruner <mab828@drexel.edu>
2021-11-29 09:01:47 -0500

    update

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,17 @@
 // import { gsap } from "gsap";
+import { gsap } from "gsap";
+import { MotionPathPlugin } from "gsap/MotionPathPlugin";
+import { DrawSVGPlugin } from "gsap/DrawSVGPlugin";
+import { GSDevTools } from "gsap/GSDevTools";
+
+gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
+
+const mainTL = gsap.timeline();
+
+mainTL.from("Ball",{duration:0.5, y:"-=500", ease:"none"})
+
+
+
+
+
+GSDevTools.create();
