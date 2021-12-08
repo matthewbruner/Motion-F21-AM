7d50ac0a16bea5c9b82c5adb75bbf38efddb82ad
matthewbruner <mab828@drexel.edu>
2021-12-07 21:40:46 -0500

    updates

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -6,9 +6,16 @@ import { GSDevTools } from "gsap/GSDevTools";
 
 gsap.registerPlugin(MotionPathPlugin, DrawSVGPlugin, GSDevTools);
 
-const mainTL = gsap.timeline();
 
-mainTL.from("Ball",{duration:0.5, y:"-=500", ease:"none"})
+const mainTL = gsap.timeline();
+gsap.set(".liveballs_class",{x:-28,y:300,alpha:0,scale:.4,transformOrigin:"50% 50%"});
+gsap.set(".letters",{alpha:100});
+
+mainTL.from("#ball",{duration:3, y:"-=400", ease:"bounce.out"});
+mainTL.from(".letters",{duration:3, y:"-=350", ease:"bounce.out", stagger:.3},"-=2");
+mainTL.to(".liveballs_class",{duration:1, alpha:1, stagger:.3, ease:"power2.inOut",y:225},"-=1");
+
+// mainTL.from("#ball",{duration:2, rotation:20})
 
 
 
